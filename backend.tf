7975cdebdaf0ae78d061ddde32accf373e646c4d
Dhanalakshmi <dhanalakshmi.bc@gmail.com>
2024-07-16 22:44:11 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hypha-test-bucket-deepu"
+    bucket = "hypha-test-bucket-deepu1"
     key    = "k8s_webapp_tf_state_file.tfstate"
     region = "ap-south-1"
   }
